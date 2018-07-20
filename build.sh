dfda826afe4048436773cdbea92dce5598e2ffd9
Jin XinXin <jinxinxin@qiniu.com>
2018-07-20 19:58:31 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+# --with-http_realip_module
+# http://nginx.org/en/docs/http/ngx_http_realip_module.html
+auto/configure --prefix=/User/jemy/Temp/nginx --with-http_realip_module
+
+# --with-http_addition_module
+# http://nginx.org/en/docs/http/ngx_http_addition_module.html
+auto/configure --prefix=/Users/jemy/Temp/nginx --with-http_addition_module
